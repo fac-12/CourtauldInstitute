88a123ed4320dbf1ecb21377cbab3ad7de7e28a4
Shannon Jensen <shannonjensen@gmail.com>
2018-02-06 16:57:57 +0000

    working getFeed query & addFeed query, with single tag

--- queries/feeds.js
+++ queries/feeds.js
@@ -1,30 +1,28 @@
 const db = require("../database/db_connection");
 
-const getFeeds = (filter, count) => {
-  db.query(
-    `SELECT feeds.id, feeds.user_id, feeds.datetime, feeds.image_url, feeds.title, feeds.content FROM feeds, feeds_tags WHERE feeds.id = feeds_tags.feed_id AND feeds_tags.tag_id = (SELECT id FROM tags WHERE tag = $1)`,
-    [filter]
-  );
-  // .then(res => {
-  //   console.log("res, ", res);
-  //   return res.slice(0, count);
-  // })
-  // .catch(err => console.log(err));
-};
+const getFeeds = (filter, count) =>
+  db
+    .query(
+      `SELECT feeds.id, feeds.user_id, feeds.datetime, feeds.image_url, feeds.title, feeds.content FROM feeds, feeds_tags WHERE feeds.id = feeds_tags.feed_id AND feeds_tags.tag_id = (SELECT id FROM tags WHERE tag = $1)`,
+      [filter]
+    )
+    .then(res => res.slice(0, count))
+    .catch(err => console.log(err));
 
-const addFeed = data => {
+const addFeed = data =>
   db
     .query(
       `INSERT INTO feeds (user_id, datetime, image_url, title, content) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
       [data.user_id, data.datetime, data.image_url, data.title, data.content]
     )
-    .then(id => {
+    .then(res =>
       db.query(
-        `INSERT INTO feeds_tags (feed_id, tag_id) VALUES (${id}, (SELECT id FROM tags WHERE tag =$1) RETURNING feed_id`,
+        `INSERT INTO feeds_tags (feed_id, tag_id) VALUES (${
+          res[0].id
+        }, (SELECT id FROM tags WHERE tag = $1)) RETURNING feed_id`,
         [data.tag]
-      );
-    })
+      )
+    )
     .catch(err => console.log(err));
-};
 
 module.exports = { getFeeds, addFeed };
--- routes/get_routes.js
+++ routes/get_routes.js
@@ -4,7 +4,6 @@ module.exports = app => {
   app.get("/api/feeds", async (req, res) => {
     try {
       const feedsData = await getFeeds(req.query.filter, req.query.count);
-      console.log("Feeds data ", feedsData);
       res.send(feedsData);
     } catch (err) {
       throw err;
