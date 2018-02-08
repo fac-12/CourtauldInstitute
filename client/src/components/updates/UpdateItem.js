ae78d95fe6a6e5ea5bd9cd555bf99100fdd067fd
Shannon Jensen <shannonjensen@gmail.com>
2018-02-08 16:45:35 +0000

    updates feed basic styling and date conversion #29 #30

--- client/src/components/updates/UpdateItem.js
+++ client/src/components/updates/UpdateItem.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { parseDateTime } from "../../helpers/conversions";
+
+const StyledDiv = styled.div`
+  margin: 10px 20px 20px 20px;
+`;
+
+const StyledTitle = styled.h2`
+  font-family: SourceSerifPro;
+  font-weight: 600;
+  font-size: 1.125rem;
+  color: #333333;
+  margin: 0.5rem 0;
+`;
+
+const StyledContent = styled.p`
+  font-family: OpenSans;
+  font-size: 0.8125rem;
+  color: #333333;
+  line-height: 1.375rem;
+  margin: 0.5rem 0;
+`;
+
+const StyledInfoDiv = styled.div`
+  margin: 10px 0;
+`;
+
+const StyledInfo = styled.p`
+  font-family: OpenSans;
+  font-style: italic;
+  font-size: 0.8125rem;
+  color: #7f8c8d;
+  margin: 5px 0;
+`;
 
 class UpdateItem extends Component {
   render() {
-    const data = this.props.data;
-    return <div key="update item">{data.title}</div>;
+    const { title, content, user_id, tags, datetime } = this.props.data;
+    const { date, time } = parseDateTime(datetime);
+    return (
+      <StyledDiv>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledContent>{content}</StyledContent>
+        <StyledInfoDiv>
+          <StyledInfo>
+            Posted by {user_id} in {tags.split(",").join(", ")}
+          </StyledInfo>
+          <StyledInfo>
+            on {date} at {time}
+          </StyledInfo>
+        </StyledInfoDiv>
+      </StyledDiv>
+    );
   }
 }
 
---
+++ client/src/helpers/conversions.js
@@ -0,0 +1,19 @@
+const zeroPadTime = input =>
+  input.toString().length < 2 ? `0${input}` : input;
+
+export const pInt = input => parseInt(input, 10);
+
+export const parseDateTime = datetime => {
+  const dateObj = new Date(pInt(datetime));
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+  const date = dateObj.toLocaleDateString("en-GB", options);
+  const time = dateObj.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+  return { date, time };
+};
