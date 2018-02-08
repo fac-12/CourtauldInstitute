d1ce538a355b28b5aa752106c30d32ac9f6af269
Gbaja <gbajaf@yahoo.co.uk>
2018-02-08 15:50:51 +0000

    Styled profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import Header from "../Header";
+import styled from "styled-components";
+
+const ProfileDiv = styled.div`
+  margin-left: 10px;
+`;
+
+const NameAndImageDiv = styled.div`
+  display: grid;
+  grid-template-columns: 45% 55%;
+  margin-top: 20px;
+`;
+
+const Name = styled.h3`
+  align-self: center;
+`;
+const StyledImage = styled.img`
+  border-radius: 100%;
+  width: 85%;
+  align-self: center;
+`;
 
 class Profile extends Component {
   render() {
     console.log(this.props.data);
     return (
-      <div>
-        <Header title={this.props.data.first_name} />
-        <p>{this.props.data.first_name}</p>
-        <p>{this.props.data.last_name}</p>
+      <ProfileDiv>
+        <Header title="Profile" />
+        <NameAndImageDiv>
+          <StyledImage src={this.props.data.picture_url} />
+          <Name>
+            {this.props.data.first_name} {this.props.data.last_name}
+          </Name>
+        </NameAndImageDiv>
+        <h3>About me</h3>
         <p>{this.props.data.about_me}</p>
+        <h3> Why I volunteer </h3>
         <p>{this.props.data.why_volunteer}</p>
+        <h3> Role </h3>
         <p>{this.props.data.type}</p>
-        <p>{this.props.data.picture_url}</p>
-      </div>
+      </ProfileDiv>
     );
   }
 }
