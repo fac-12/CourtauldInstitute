1b3df3fe479e797be914fa1b3dd200e50101d11b
Shannon Jensen <shannonjensen@gmail.com>
2018-02-12 19:49:36 +0000

    make styles universally accessible

---
+++ client/src/components/styledDefaults/FormStyles.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 20px;
+  font-family: "Open Sans", sans-serif;
+`;
+
+export const StyledInput = styled.input`
+  border: 2px;
+  border-style: inset;
+  border-color: #ecf0f1;
+  margin: 5px 0 10px 0;
+  width: 100%;
+`;
+
+export const StyledCheckbox = styled.input`
+  margin: 5px 5px 2px 0;
+  position: relative;
+  top: 1px;
+`;
+export const StyledTextArea = styled.textarea`
+  border: 2px;
+  border-style: inset;
+  border-color: #ecf0f1;
+  margin: 5px 0 10px 0;
+  width: 100%;
+  height: 100px;
+`;
+export const StyledFileUpload = StyledInput.extend`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+`;
+export const StyledSubmit = styled.button`
+  border: 1px solid #c9cccc;
+  background: #ffffff;
+  border-radius: 8px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 0.75rem;
+  color: #333333;
+  padding: 5px 10px;
+  margin: 10px 0;
+  outline: none;
+  text-decoration: none;
+  display: block;
+  text-align: center;
+`;
+
+export const StyledErrorDiv = styled.div`
+  color: red;
+`;
+
+export const StyledFileUploadBtn = styled.label`
+  border: 1px solid #c9cccc;
+  background: #ffffff;
+  border-radius: 8px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 0.75rem;
+  color: #333333;
+  padding: 5px 10px;
+  margin: 10px 0;
+  outline: none;
+  text-decoration: none;
+  display: block;
+  text-align: center;
+  cursor: pointerl;
+`;
+
+export const StyledP = styled.p`
+  margin: 5px 0;
+  font-family: "Source Serif Pro", serif;
+  font-weight: 600;
+`;
+export const StyledLabel = styled.label`
+  font-family: "Source Serif Pro", serif;
+  font-weight: 600;
+`;
+export const StyledTagsLabel = styled.label`
+  font-size: 0.9rem;
+`;
--- client/src/components/updates/AddUpdateForm.js
+++ client/src/components/updates/AddUpdateForm.js
@@ -1,89 +1,19 @@
 import React, { Component } from "react";
 import { Field } from "redux-form";
 import styled from "styled-components";
-
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin: 20px 20px;
-  font-family: "Open Sans", sans-serif;
-`;
-
-const StyledInput = styled.input`
-  border: 2px;
-  border-style: inset;
-  border-color: #ecf0f1;
-  margin: 5px 0 10px 0;
-  width: 100%;
-`;
-const StyledCheckbox = styled.input`
-  margin: 5px 5px 2px 0;
-  position: relative;
-  top: 1px;
-`;
-const StyledTextArea = styled.textarea`
-  border: 2px;
-  border-style: inset;
-  border-color: #ecf0f1;
-  margin: 5px 0 10px 0;
-  width: 100%;
-  height: 100px;
-`;
-const StyledFileUpload = StyledInput.extend`
-  width: 0.1px;
-  height: 0.1px;
-  opacity: 0;
-  overflow: hidden;
-  position: absolute;
-  z-index: -1;
-`;
-const StyledSubmit = styled.button`
-  border: 1px solid #c9cccc;
-  background: #ffffff;
-  border-radius: 8px;
-  font-family: "Open Sans", sans-serif;
-  font-size: 0.75rem;
-  color: #333333;
-  padding: 5px 10px;
-  margin: 10px 0;
-  outline: none;
-  text-decoration: none;
-  display: block;
-  text-align: center;
-`;
-
-const StyledErrorDiv = styled.div`
-  color: red;
-`;
-
-const StyledFileUploadBtn = styled.label`
-  border: 1px solid #c9cccc;
-  background: #ffffff;
-  border-radius: 8px;
-  font-family: "Open Sans", sans-serif;
-  font-size: 0.75rem;
-  color: #333333;
-  padding: 5px 10px;
-  margin: 10px 0;
-  outline: none;
-  text-decoration: none;
-  display: block;
-  text-align: center;
-  cursor: pointerl;
-`;
-
-const StyledP = styled.p`
-  margin: 5px 0;
-  font-family: "Source Serif Pro", serif;
-  font-weight: 600;
-`;
-const StyledLabel = styled.label`
-  font-family: "Source Serif Pro", serif;
-  font-weight: 600;
-`;
-const StyledTagsLabel = styled.label`
-  font-size: 0.9rem;
-`;
+import {
+  StyledForm,
+  StyledInput,
+  StyledCheckbox,
+  StyledTextArea,
+  StyledFileUpload,
+  StyledSubmit,
+  StyledErrorDiv,
+  StyledFileUploadBtn,
+  StyledP,
+  StyledLabel,
+  StyledTagsLabel
+} from "../styledDefaults/FormStyles";
 
 class AddUpdateForm extends Component {
   adaptFileEventToValue = delegate => e => delegate(e.target.files[0]);
