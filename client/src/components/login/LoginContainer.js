9e07926418ed5970bd1b450dd395d7b6fb90e292
Jemila <jemila.abulhawa@gmail.com>
2018-02-12 11:55:28 +0000

    #14 Add initial styling to the login page

--- client/src/components/login/LoginContainer.js
+++ client/src/components/login/LoginContainer.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import LoginForm from "./LoginForm";
+import LoginBackground from "./loginBackground.png";
+
+import styled from "styled-components";
+
+const StyledLoginContainer = styled.div`
+  background-image: url(${LoginBackground});
+  background-size: contain;
+`;
+
+const Login = styled.div`
+  font-family: "Source Serif Pro", serif;
+  border: 1px solid red;
+  height: 100vh;
+  background-size: contain;
+  color: #ecf0f1;
+  margin-left: 50px;
+`;
+
+const LoginHeader = styled.h1`
+  border: 1px solid red;
+  font-size: 25px;
+  font-weight: normal;
+  margin: 0 auto;
+  text-align: center;
+`;
 
 class LoginContainer extends Component {
   render() {
     return (
-      <div>
-        <h1>Volunteer Platform</h1>
-        <LoginForm />
-      </div>
+      <StyledLoginContainer>
+        <Login>
+          <LoginHeader>Volunteer Platform</LoginHeader>
+          <LoginForm />
+        </Login>
+      </StyledLoginContainer>
     );
   }
 }
