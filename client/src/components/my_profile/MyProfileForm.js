dea449798cfcb971ff591a8f7bc1bc05f9f401e4
Gbaja <gbajaf@yahoo.co.uk>
2018-02-12 17:38:22 +0000

    set up my profile redux

---
+++ client/src/components/my_profile/MyProfileForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Field } from "redux-form";
+
+class MyProfileContainer extends Component {
+  render() {
+    const { handleSubmit, onSubmit } = this.props;
+    console.log(this.props);
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <img src={this.props.picture_url} />
+        <Field name="first_name" component="input" />
+        <Field name="last_name" component="input" />
+        <Field name="email" component="input" />
+        <Field name="why_volunteer" component="textarea" />
+        <Field name="about_me" component="textarea" />
+        <button type="submit">Update profile</button>
+      </form>
+    );
+  }
+}
+
+export default MyProfileContainer;
