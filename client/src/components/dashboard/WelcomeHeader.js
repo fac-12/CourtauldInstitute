eec40033a7ada13e72acbb25a398ceff33b857af
Jemila <jemila.abulhawa@gmail.com>
2018-02-15 12:56:00 +0000

    Add max width to progress update

--- client/src/components/dashboard/WelcomeHeader.js
+++ client/src/components/dashboard/WelcomeHeader.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const StyledWelcomeHeader = styled.h1`
-  font-size: 24px;
+  font-size: 1.5rem;
   margin-left: 1.5rem;
 `;
 
 const StyledName = styled.span`
   font-size: 50px;
+  /* @media screen and (min-width: 400px) {
+    display: block;
+    text-align: center;
+  } */
 `;
 
 class WelcomeHeader extends Component {
