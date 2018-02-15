6b9d2a889e9c16b204734c42f8d208158e169ef5
Jemila <jemila.abulhawa@gmail.com>
2018-02-15 16:30:55 +0000

    update progress initial CSS

--- client/src/components/dashboard/ProgressUpdate.js
+++ client/src/components/dashboard/ProgressUpdate.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const StyledProgressUpdate = styled.div`
-  margin-left: 1.5rem;
-  margin-right: 1.5rem;
-  font-size: 1.05rem;
+  margin: 0 1.5rem;
+  font-size: 0.9rem;
 
   @media screen and (min-width: 960px) {
     max-width: 500px;
@@ -15,14 +14,45 @@ const StyledP = styled.p`
   margin-bottom: 0.5rem;
 `;
 
+const Progress = styled.div`
+  display: flex;
+`;
+
+const StyledPCollection = styled.p`
+  margin: 10px 0;
+`;
+
+const StyledWeekProgressData = styled.p`
+  font-size: 2rem;
+`;
+
+const StyledWeekProgressText = styled.p`
+  font-size: 0.85rem;
+  margin-bottom: 10px;
+`;
+
+const StyledOverall = StyledWeekProgressData.extend`
+  margin-left: 30px;
+`;
+
 class ProgressUpdate extends Component {
   render() {
     return (
       <StyledProgressUpdate>
         <StyledP>{this.props.text}</StyledP>
-        <StyledP>{this.props.collection}</StyledP>
-        <StyledP>{this.props.weekProgress}</StyledP>
-        <StyledP>{this.props.overallProgress}</StyledP>
+        <StyledPCollection>
+          {this.props.collection} Collection
+        </StyledPCollection>
+        <Progress>
+          <StyledWeekProgressData>
+            {this.props.weekProgress}
+            <StyledWeekProgressText>completed last week</StyledWeekProgressText>
+          </StyledWeekProgressData>
+          <StyledOverall>
+            {this.props.overallProgress}
+            <StyledWeekProgressText>completed overall</StyledWeekProgressText>
+          </StyledOverall>
+        </Progress>
       </StyledProgressUpdate>
     );
   }
