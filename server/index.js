652f0a46f17690c1714e6afea5cc5532c83a4ca6
Shannon Jensen <shannonjensen@gmail.com>
2018-02-16 06:21:00 +0000

    try solution to make router and heroku play nice

--- server/index.js
+++ server/index.js
@@ -16,17 +16,25 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000
   })
 );
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", express.static("client/build"));
+}
 
 require("./routes/get_routes")(app);
 require("./routes/post_routes")(app);
 require("./routes/put_routes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+});
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.disabled("x-powered-by");
 
